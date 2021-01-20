e00f40611f06cdcc4428a5ba7f1f943cffcab093
linqing51 <37890017+linqing51@users.noreply.github.com>
2021-01-20 23:58:22 +0800

    添加预设方案

---
+++ MCU_Application/deviceInfo/preScheme.c
@@ -0,0 +1,27 @@
+#include "preScheme.h"
+/*****************************************************************************/
+char const PRE_SCHEME_TABLE[16][CONFIG_MAX_SCHEME_NAME_SIZE]  = 
+									   {{"EVLA Thigh"},
+										{"EVLA calf"},
+										{"EVLA Small Vessel"},
+										{"Hemorrhoids Grade 2 250 Joules per hemorrhoid"},
+										{"Hemorrhoids Grade 3 350 Joules per hemorrhoid"},
+										{"Hemorrhoids Grade 4 350 Joules per hemorrhoid"},
+										{"Fistula"},
+										{"Pilonidal Sinus"},
+										{"Fissure Abaltion"},
+										{"PLDD L2-L3, L3-L4, L5-S1 Total 800-1500J"},
+										{"PLDD L4-L5 Total 800-1800J"},
+										{"NULL"},
+										{"NULL"},
+										{"NULL"},
+										{"NULL"},
+										{"NULL"}};
+
+										
+										
+										
+////
+
+
+										
